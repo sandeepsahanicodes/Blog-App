74f04993d7866bd9f7db3cec83af986084c293a1
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2025-03-08 17:40:14 +0530

    chore: environment variable setup

---
+++ src/conf/conf.js
@@ -0,0 +1,10 @@
+import { AppwriteException } from "appwrite";
+
+const conf = {
+  appwriteUrl: String(process.env.REACT_APP_APPWRITE_URL),
+  appwriteProjectId: String(process.env.REACT_APP_APPWRITE_PROJECT_ID),
+  appwiteDatabaseId: String(process.env.REACT_APP_APPWRITE_DATABASE_ID),
+  appwriteCollectionId: String(process.env.REACT_APP_APPWRITE_COLLECTION_ID),
+  appwriteBucketId: String(process.env.REACT_APP_APPWRITE_BUCKET_ID)
+};
+export default conf;
