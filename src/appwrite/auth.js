88b76876da2fa4b89b5f99a01ccd95eff5cdc09b
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2025-03-08 18:40:37 +0530

    feat: add authentication functionality

---
+++ src/appwrite/auth.js
@@ -0,0 +1,60 @@
+import conf from "../conf/conf.js";
+import { Client, Account } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        return this.login();
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser() :: ", error);
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite service :: logout() :: ", error);
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
