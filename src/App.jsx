8ee57d37aa5379edebb56a8cf7ba5678b3caa085
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2025-03-09 20:17:44 +0530

    feat: add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import "./App.css";
 
 function App() {
- 
   return (
-    <>
-      <h1>Blog App under development</h1>
-    </>
-  )
+    <div>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,86 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
-createRoot(document.getElementById('root')).render(
+import Home from "./pages/Home.jsx";
+import App from "../src/App.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import AllPosts from "./pages/AllPosts.jsx";
+import EditPost from "./pages/EditPost.jsx"
+import Post from "./pages/Post.jsx";
+import AddPost from "./pages/AddPost.jsx";
+
+import Protected from "./components/AuthLayout.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: (
+          <Protected authentication={false}>
+            <Login />
+          </Protected>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <Protected authentication={false}>
+            <Signup />
+          </Protected>
+        ),
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <Protected authentication>
+            <AddPost />
+          </Protected>
+        ),
+      },
+      {
+        path: "/add-post",
+        element: (
+          <Protected authentication>
+            <AllPosts />
+          </Protected>
+        ),
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (
+          <Protected authentication>
+            <EditPost />
+          </Protected>
+        ),
+      },
+      {
+        path: "/post/:slug",
+        element: (
+          <Protected authentication>
+            <Post />
+          </Protected>
+        ),
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
