2232092ee931a4226d8c6bfa375001312bd1af4a
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2025-03-20 03:26:54 +0530

    feat: add api key in the tinymce editor and AI assistant option

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Controller } from "react-hook-form";
 import { Editor } from "@tinymce/tinymce-react";
+import conf from "../conf/conf";
 
-function RTE({ name, control, label, defaultValue }) {
+function RTE({ name, control, label, defaultValue = "" }) {
   return (
     <div className="w-full">
       {label && <label className="inline-block mb-1 pl-1">{label}</label>}
@@ -11,6 +12,7 @@ function RTE({ name, control, label, defaultValue }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
+            apiKey={conf.tinyMceApiKey}
             initialValue={defaultValue}
             init={{
               branding: false,
@@ -63,6 +65,16 @@ function RTE({ name, control, label, defaultValue }) {
               ],
               toolbar:
                 "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+              tinycomments_mode: "embedded",
+              tinycomments_author: "Author name",
+              mergetags_list: [
+                { value: "First.Name", title: "First Name" },
+                { value: "Email", title: "Email" },
+              ],
+              ai_request: (request, respondWith) =>
+                respondWith.string(() =>
+                  Promise.reject("See docs to implement AI Assistant")
+                ),
             }}
             onEditorChange={onChange}
           />
